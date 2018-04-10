15a0c9bc304f527f1c9f6d601121752501c2fecf
zhuzhang <554961203@qq.com>
2018-04-10 16:51:46 +0800

    upgrade

--- model_open.py
+++ model_open.py
@@ -3,6 +3,12 @@ sys.path.append('./qa_utils/')
 import tensorflow as tf
 import layers
 import random
+import pickle as pkl
+import utils
+
+def load_file(filename):
+    with open(filename,'rb') as f1:
+        return pkl.load(f1)
 
 class Model(object):
 
@@ -18,6 +24,7 @@ class Model(object):
 
         self.ref_dim =  model_params['ref_dim']
         self.lstm_dim = model_params['lstm_dim']
+        self.second_lstm_dim = model_params['second_lstm_dim']
         self.attention_dim = model_params['attention_dim']
         self.regularization_beta = model_params['regularization_beta']
         self.dropout_prob = model_params['dropout_prob']
@@ -35,7 +42,6 @@ class Model(object):
         self.y_mask = tf.placeholder(tf.float32, [None, self.max_n_a_words])
         self.ans_vec = tf.placeholder(tf.float32, [None, self.max_n_a_words, self.input_ques_dim])
         self.batch_size = tf.placeholder(tf.int32, [])
-
         self.is_training = tf.placeholder(tf.bool)
         
         self.Wsi = tf.get_variable('Wsi', shape=[self.input_frame_dim, self.ref_dim], dtype=tf.float32,
@@ -49,19 +55,18 @@ class Model(object):
         self.Vs = tf.get_variable('Vs', shape=[self.ref_dim,1], dtype=tf.float32,
                                     initializer=tf.contrib.layers.xavier_initializer())
 
-        input_x = tf.contrib.layers.dropout(self.input_x, self.dropout_prob, is_training=self.is_training)
+        self.input_x = tf.contrib.layers.dropout(self.input_x, self.dropout_prob, is_training=self.is_training)
 
         # Question LSTM layer, [n_steps * (batch_size, input_dim)] -> [n_steps * (batch_size, 2*lstm_dim)]
-        input_q = tf.contrib.layers.dropout(self.input_q, self.dropout_prob, is_training=self.is_training)
-        q_lstm_output, q_lstm_state = layers.dynamic_origin_lstm_layer(input_q, self.lstm_dim, 'q_lstm', input_len=self.input_q_len)
-        q_lstm_output = tf.contrib.layers.dropout(q_lstm_output, self.dropout_prob, is_training=self.is_training)
-        q_last_state = tf.contrib.layers.dropout(q_lstm_state[1], self.dropout_prob, is_training=self.is_training)
+        self.input_q = tf.contrib.layers.dropout(self.input_q, self.dropout_prob, is_training=self.is_training)
+        q_lstm_output, q_lstm_state = layers.dynamic_origin_lstm_layer(self.input_q, self.lstm_dim, 'q_lstm', input_len=self.input_q_len)
+        self.q_last_state = tf.contrib.layers.dropout(q_lstm_state[1], self.dropout_prob, is_training=self.is_training)
 
 
         cell = tf.contrib.rnn.BasicLSTMCell(self.lstm_dim, forget_bias=0.0, state_is_tuple=True)
         state = cell.zero_state(self.batch_size, tf.float32)
 
-        cell_second = tf.contrib.rnn.BasicLSTMCell(self.lstm_dim, forget_bias=0.0, state_is_tuple=True)
+        cell_second = tf.contrib.rnn.BasicLSTMCell(self.second_lstm_dim, forget_bias=0.0, state_is_tuple=True)
         state_second = cell_second.zero_state(self.batch_size, tf.float32)
 
         img_first_outputs = []
@@ -96,19 +101,22 @@ class Model(object):
 
         mask_output = tf.concat(mask_output,1)
 
-        v_first_lstm_output = tf.reshape(tf.concat(img_first_outputs, 1), [-1,self.input_n_frames-1, self.lstm_dim])
-        v_second_lstm_output = tf.reshape(tf.concat(img_second_outputs, 1), [-1,self.input_n_frames-1, self.lstm_dim])
+        self.v_first_lstm_output = tf.reshape(tf.concat(img_first_outputs, 1), [-1,self.input_n_frames-1, self.lstm_dim])
+        self.v_second_lstm_output = tf.reshape(tf.concat(img_second_outputs, 1), [-1,self.input_n_frames-1, self.lstm_dim])
+        self.v_first_lstm_output = tf.contrib.layers.dropout(self.v_first_lstm_output, self.dropout_prob,is_training=self.is_training)
 
-        v_first_attention_output, first_attention_score = layers.matrix_attention_layer(v_first_lstm_output, q_last_state, self.attention_dim, 'v_first_attention')
-        v_second_attention_output, second_attention_score = layers.mask_matrix_attention_layer(v_second_lstm_output, q_last_state, self.attention_dim, mask_output, 'v_second_attention')
 
-        concat_output = tf.concat([q_last_state, v_first_attention_output, v_second_attention_output], axis=1)
+        self.v_first_attention_output, self.first_attention_score = layers.matrix_attention_layer(self.v_first_lstm_output, self.q_last_state, self.attention_dim, 'v_first_attention')
+        self.v_second_attention_output, self.second_attention_score = layers.mask_matrix_attention_layer(self.v_second_lstm_output, self.q_last_state, self.attention_dim, mask_output, 'v_second_attention')
+
+        concat_output = tf.concat([self.q_last_state, self.v_first_attention_output], axis=1)
 
 
         # decoder
         ######
         # output -> first_atten
-        self.decoder_cell = tf.contrib.rnn.BasicLSTMCell(self.decode_dim)
+        # self.decoder_cell = tf.contrib.rnn.BasicLSTMCell(self.decode_dim)
+        # self.decoder_cell = tf.contrib.rnn.GRUCell(self.decode_dim)
 
         with tf.variable_scope('linear'):
             decoder_input_W = tf.get_variable('w', shape=[concat_output.shape[1], self.decode_dim], dtype=tf.float32, initializer=tf.contrib.layers.xavier_initializer()) # initializer=tf.random_normal_initializer(stddev=0.03))
@@ -123,9 +131,32 @@ class Model(object):
         self.word_to_lstm_w = tf.Variable(tf.random_uniform([self.input_ques_dim, self.decode_dim], -0.1, 0.1), name='word_to_lstm_W')
         self.word_to_lstm_b = tf.Variable(tf.random_uniform([self.decode_dim], -0.1, 0.1), name='word_to_lstm_b')
 
+        # decoder attention layer
+        with tf.variable_scope('decoder_attention'):
+            self.attention_w_q = tf.get_variable('attention_w_q', shape=[self.lstm_dim, self.attention_dim], dtype=tf.float32,
+                                  initializer=tf.contrib.layers.xavier_initializer())
+            self.attention_w_x = tf.get_variable('attention_w_x', shape=[self.decode_dim, self.attention_dim], dtype=tf.float32,
+                                  initializer=tf.contrib.layers.xavier_initializer())
+            self.attention_w_h = tf.get_variable('attention_w_h', shape=[self.lstm_dim,self.attention_dim], dtype=tf.float32,
+                                  initializer=tf.contrib.layers.xavier_initializer())
+            self.attention_b = tf.get_variable('attention_b', shape=[self.attention_dim], dtype=tf.float32,
+                                initializer=tf.contrib.layers.xavier_initializer())
+            self.attention_a = tf.get_variable('attention_a', shape=[self.attention_dim, 1], dtype=tf.float32,
+                                initializer=tf.contrib.layers.xavier_initializer())
+            self.attention_to_decoder = tf.get_variable('attention_to_decoder', shape=[self.lstm_dim, self.decode_dim], dtype=tf.float32,
+                                initializer=tf.contrib.layers.xavier_initializer())
+        # decoder
+        with tf.variable_scope('decoder'):
+            self.decoder_r = tf.get_variable('decoder_r', shape=[self.decode_dim*3, self.decode_dim], dtype=tf.float32,
+                                  initializer=tf.contrib.layers.xavier_initializer())
+            self.decoder_z = tf.get_variable('decoder_z', shape=[self.decode_dim*3, self.attention_dim], dtype=tf.float32,
+                                  initializer=tf.contrib.layers.xavier_initializer())
+            self.decoder_w = tf.get_variable('decoder_w', shape=[self.decode_dim*3,self.attention_dim], dtype=tf.float32,
+                                  initializer=tf.contrib.layers.xavier_initializer())
+
+
         # embedding layer
-        import numpy as np
-        embeddings = np.load('/home1/xsw/mm_2018/data/tsn_score/caption/wordvec.npy')
+        embeddings = load_file(self.data_params['word_embedding'])
         self.Wemb = tf.constant(embeddings, dtype=tf.float32)
 
         # generate training
@@ -146,7 +177,7 @@ class Model(object):
     def generate_answer_on_training(self):
         with tf.variable_scope("decoder"):
             answer_train = []
-            decoder_state = self.decoder_cell.zero_state(self.batch_size, tf.float32)
+            decoder_state = tf.constant(value=0.0,shape=[self.batch_size,self.decode_dim])
             loss = 0.0
 
             with tf.variable_scope("lstm") as scope:
@@ -155,12 +186,32 @@ class Model(object):
                         current_emb = self.decoder_input
                     else:
                         scope.reuse_variables()
-                        next_word_vec = tf.nn.embedding_lookup(self.Wemb, max_prob_word)
-                        current_emb = tf.nn.xw_plus_b(next_word_vec, self.word_to_lstm_w, self.word_to_lstm_b)
-                        #current_emb = tf.nn.xw_plus_b(self.ans_vec[:, i-1, :], self.word_to_lstm_w, self.word_to_lstm_b)
+                        # next_word_vec = tf.nn.embedding_lookup(self.Wemb, max_prob_word)
+                        # current_emb = tf.nn.xw_plus_b(next_word_vec, self.word_to_lstm_w, self.word_to_lstm_b)
+                        current_emb = tf.nn.xw_plus_b(self.ans_vec[:, i-1, :], self.word_to_lstm_w, self.word_to_lstm_b)
 
-                    # LSTM
-                    output, decoder_state = self.decoder_cell(current_emb, decoder_state) 
+
+                    # decoder_state
+                    tiled_decoder_state_h = tf.tile(tf.expand_dims(decoder_state, 1), tf.stack([1, self.input_n_frames-1, 1]))
+                    tiled_q_last_state = tf.tile(tf.expand_dims(self.q_last_state, 1),tf.stack([1, self.input_n_frames - 1, 1]))
+                    attention_input = tf.tanh(utils.tensor_matmul(self.v_first_lstm_output, self.attention_w_x)
+                                              + utils.tensor_matmul(tiled_q_last_state, self.attention_w_q)
+                                              + utils.tensor_matmul(tiled_decoder_state_h, self.attention_w_h)
+                                              +self.attention_b)
+                    attention_score = tf.nn.softmax(tf.squeeze(utils.tensor_matmul(attention_input, self.attention_a), axis=[2]))
+                    attention_output = tf.reduce_sum(tf.multiply(self.v_first_lstm_output, tf.expand_dims(attention_score, 2)), 1)
+                    attention_decoder = tf.matmul(attention_output,self.attention_to_decoder)
+
+                    # decoder : GRU with attention
+                    decoder_input = tf.concat([decoder_state,attention_decoder,current_emb],axis=1)
+                    decoder_r_t = tf.nn.sigmoid(tf.matmul(self.decoder_r, decoder_input))
+                    decoder_z_t = tf.nn.sigmoid(tf.matmul(self.decoder_z, decoder_input))
+                    decoder_middle = tf.concat([tf.multiply(decoder_r_t,decoder_state),tf.multiply(decoder_r_t,attention_decoder,current_emb)],axis=1)
+                    decoder_state_ = tf.tanh(tf.matmul(self.decoder_w,decoder_middle))
+                    decoder_state = tf.multiply((1-decoder_z_t),decoder_state) + tf.multiply(decoder_z_t,decoder_state_)
+
+                    output = decoder_state
+
 
                     # ground truth
                     labels = tf.expand_dims(self.y[:, i], 1)
@@ -183,7 +234,7 @@ class Model(object):
     def generate_answer_on_testing(self):
         with tf.variable_scope("decoder"):
             answer_test = []
-            decoder_state = self.decoder_cell.zero_state(self.batch_size, tf.float32)
+            decoder_state = tf.constant(value=0.0, shape=[self.batch_size, self.decode_dim])
             loss = 0.0
 
             with tf.variable_scope("lstm") as scope:
@@ -195,8 +246,30 @@ class Model(object):
                         next_word_vec = tf.nn.embedding_lookup(self.Wemb, max_prob_word)
                         current_emb = tf.nn.xw_plus_b(next_word_vec, self.word_to_lstm_w, self.word_to_lstm_b)
 
-                    # LSTM
-                    output, decoder_state = self.decoder_cell(current_emb, decoder_state) 
+                    # decoder_state
+                    tiled_decoder_state_h = tf.tile(tf.expand_dims(decoder_state, 1),
+                                                    tf.stack([1, self.input_n_frames - 1, 1]))
+                    tiled_q_last_state = tf.tile(tf.expand_dims(self.q_last_state, 1),
+                                                 tf.stack([1, self.input_n_frames - 1, 1]))
+                    attention_input = tf.tanh(utils.tensor_matmul(self.v_first_lstm_output, self.attention_w_x)
+                                              + utils.tensor_matmul(tiled_q_last_state, self.attention_w_q)
+                                              + utils.tensor_matmul(tiled_decoder_state_h, self.attention_w_h)
+                                              + self.attention_b)
+                    attention_score = tf.nn.softmax(tf.squeeze(utils.tensor_matmul(attention_input, self.attention_a), axis=[2]))
+                    attention_output = tf.reduce_sum(tf.multiply(self.v_first_lstm_output, tf.expand_dims(attention_score, 2)), 1)
+                    attention_decoder = tf.matmul(attention_output, self.attention_to_decoder)
+
+                    # decoder : GRU with attention
+                    decoder_input = tf.concat([decoder_state, attention_decoder, current_emb], axis=1)
+                    decoder_r_t = tf.nn.sigmoid(tf.matmul(self.decoder_r, decoder_input))
+                    decoder_z_t = tf.nn.sigmoid(tf.matmul(self.decoder_z, decoder_input))
+                    decoder_middle = tf.concat([tf.multiply(decoder_r_t, decoder_state),
+                                                tf.multiply(decoder_r_t, attention_decoder, current_emb)], axis=1)
+                    decoder_state_ = tf.tanh(tf.matmul(self.decoder_w, decoder_middle))
+                    decoder_state = tf.multiply((1 - decoder_z_t), decoder_state) + tf.multiply(decoder_z_t,
+                                                                                                decoder_state_)
+
+                    output = decoder_state
 
                     # ground truth
                     labels = tf.expand_dims(self.y[:, i], 1) 
